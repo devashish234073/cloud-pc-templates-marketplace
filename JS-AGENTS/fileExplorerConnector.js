c73c2fdba8eab42bf7fa206e1d4f7e96f0c88240
devashish234073 <devashish234073gmail.com>
2026-02-13 00:23:18 +0530

    fixed cors

--- JS-AGENTS/fileExplorerConnector.js
+++ JS-AGENTS/fileExplorerConnector.js
@@ -97,6 +97,9 @@ function findFileByExactName(dir, filename, excludeFolders, results = []) {
 /* ------------------ SERVER ------------------ */
 
 const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     const parsedUrl = url.parse(req.url, true);
 
     /* -------- HEALTH -------- */
